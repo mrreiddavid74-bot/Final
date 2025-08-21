e05a8e77db76e05d41d051de48de339f6cdb6431
Ashraful <smashrafulcse@gmail.com>
2025-08-21 17:27:48 +0600

    deployment error debug

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,7 +1,5 @@
 /// <reference types="next" />
 /// <reference types="next/image-types/global" />
 
-/**
- * This file ensures Next.js types are picked up by TypeScript.
- * Do not edit directly.
- */
+// NOTE: This file should not be edited
+// see https://nextjs.org/docs/basic-features/typescript for more information.
